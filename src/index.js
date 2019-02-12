edafeff8cdd0cd5aa961cae459ef940ead2dab27
Krystian Pietroń <krystianpietr@gmail.com>
2019-02-12 09:03:17 +0100

    clear files and add imports to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Auth from './components/Auth'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { store } from './store'
+import { Provider } from 'react-redux'
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Auth>
+            <MuiThemeProvider>
+                <App />
+            </MuiThemeProvider>
+        </Auth>
+    </Provider>,
+    document.getElementById('root')
+);
+
+
